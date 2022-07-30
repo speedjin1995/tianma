8c0b8bd43d72fc8a021d5fd7e287a67b8e7bec2d
laiwoontzer <darrenlai95@gmail.com>
2022-07-30 12:34:45 +0800

    done

--- php/loadMoistures.php
+++ php/loadMoistures.php
@@ -23,7 +23,7 @@ $records = mysqli_fetch_assoc($sel);
 $totalRecords = $records['allcount'];
 
 ## Total number of record with filtering
-$sel = mysqli_query($db,"select count(*) as allcount from weighing WHERE parent_no <> '0' AND weighing.grade=grades.id".$searchQuery);
+$sel = mysqli_query($db,"select count(*) as allcount from weighing, grades WHERE parent_no <> '0' AND weighing.grade=grades.id".$searchQuery);
 $records = mysqli_fetch_assoc($sel);
 $totalRecordwithFilter = $records['allcount'];
 
