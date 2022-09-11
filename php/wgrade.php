b2f8c175bdaf1c1b425f4925911e2280200d7616
speedjin1995 <speedjin1995@gmail.com>
2022-09-11 21:29:38 +0800

    fix removed but hit error issue

--- php/wgrade.php
+++ php/wgrade.php
@@ -146,137 +146,139 @@ $_POST['newNetWeight'], $_POST['moistureAfGrade'], $_POST['parentId'], $_POST['n
         <body>';
 
         for($i=0; $i<sizeof($newLotNo); $i++){
-            $reason = null;
+            if($newLotNo[$i] != null){
+                $reason = null;
 
-            if($newReason[$i] != null && $newReason[$i] != ""){
-                $reason = $newReason[$i];
-            }
-
-            if ($insert_stmt = $db->prepare("INSERT INTO weighing (item_types, gross_weight, lot_no, tray_weight, tray_no, net_weight, grade, parent_no, pieces, grading_gross_weight, grading_net_weight, moisture_after_grading, status, reasons, remark, grading_datetime) VALUES (?, ?, ?, ?, ? ,?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")) {
-                $insert_stmt->bind_param('ssssssssssssssss', $itemType, $newGrossWeight[$i], $newLotNo[$i], $newTrayWeight[$i], $newTrayNo[$i], $newNetWeight[$i], $newGrade[$i], $parentId, $qty[$i], $newGrossWeight[$i], $newNetWeight[$i], $moistureAfGrade[$i], $newStatus[$i], $reason, $newRemark[$i], $gradingDateTime);
-                
-                // Execute the prepared query.
-                if (! $insert_stmt->execute()) {
-                    $success = false;
+                if($newReason[$i] != null && $newReason[$i] != ""){
+                    $reason = $newReason[$i];
                 }
-                else{
 
-                    $action = "User : ".$name." Add new Lot No : ".$newLotNo[$i]." And Tray No : ".$newTrayNo[$i]." in grades table!";
-
-                    if ($log_insert_stmt = $db->prepare("INSERT INTO log (userId, userName, action) VALUES (?, ?, ?)")) {
-                        $log_insert_stmt->bind_param('sss', $userID, $name, $action);
-                        
-                        if (!$log_insert_stmt->execute()) {
-                            echo json_encode(
-                                array(
-                                    "status"=> "failed", 
-                                    "message"=> $log_insert_stmt->error 
-                                )
-                            );
-                        }
-                        else{
-                            $log_insert_stmt->close();
-                        }
-                    }
-
-                    if($count > 0){
-                        $message .= '<p style="page-break-after:always;"></p>';
-                    }
-
-                    $text = "php/qrprotrait.php?id=".$insert_stmt->insert_id;;
-                    $path = 'receivesLabel/';
-                    $file = $path.uniqid().".png";
+                if ($insert_stmt = $db->prepare("INSERT INTO weighing (item_types, gross_weight, lot_no, tray_weight, tray_no, net_weight, grade, parent_no, pieces, grading_gross_weight, grading_net_weight, moisture_after_grading, status, reasons, remark, grading_datetime) VALUES (?, ?, ?, ?, ? ,?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")) {
+                    $insert_stmt->bind_param('ssssssssssssssss', $itemType, $newGrossWeight[$i], $newLotNo[$i], $newTrayWeight[$i], $newTrayNo[$i], $newNetWeight[$i], $newGrade[$i], $parentId, $qty[$i], $newGrossWeight[$i], $newNetWeight[$i], $moistureAfGrade[$i], $newStatus[$i], $reason, $newRemark[$i], $gradingDateTime);
                     
-                    // $ecc stores error correction capability('L')
-                    $ecc = 'L';
-                    $pixel_Size = 10;
-                    $frame_Size = 10;
-                    
-                    // Generates QR Code and Stores it in directory given
-                    QRcode::png($text, $file, $ecc, $pixel_Size, $frame_Size);
+                    // Execute the prepared query.
+                    if (! $insert_stmt->execute()) {
+                        $success = false;
+                    }
+                    else{
 
-                    if ($update_stmt = $db->prepare("SELECT * FROM grades WHERE id=?")) {
-                        $update_stmt->bind_param('s', $newGrade[$i]);
+                        $action = "User : ".$name." Add new Lot No : ".$newLotNo[$i]." And Tray No : ".$newTrayNo[$i]." in grades table!";
+
+                        if ($log_insert_stmt = $db->prepare("INSERT INTO log (userId, userName, action) VALUES (?, ?, ?)")) {
+                            $log_insert_stmt->bind_param('sss', $userID, $name, $action);
+                            
+                            if (!$log_insert_stmt->execute()) {
+                                echo json_encode(
+                                    array(
+                                        "status"=> "failed", 
+                                        "message"=> $log_insert_stmt->error 
+                                    )
+                                );
+                            }
+                            else{
+                                $log_insert_stmt->close();
+                            }
+                        }
+
+                        if($count > 0){
+                            $message .= '<p style="page-break-after:always;"></p>';
+                        }
+
+                        $text = "php/qrprotrait.php?id=".$insert_stmt->insert_id;;
+                        $path = 'receivesLabel/';
+                        $file = $path.uniqid().".png";
                         
-                        // Execute the prepared query.
-                        if ($update_stmt->execute()) {
-                            $result = $update_stmt->get_result();
+                        // $ecc stores error correction capability('L')
+                        $ecc = 'L';
+                        $pixel_Size = 10;
+                        $frame_Size = 10;
+                        
+                        // Generates QR Code and Stores it in directory given
+                        QRcode::png($text, $file, $ecc, $pixel_Size, $frame_Size);
 
-                            if($row = $result->fetch_assoc()){
-                                $message = '<table style="width: 640px;height: 27px;">
-                                <tr>
-                                    <td colspan="2">
-                                        <h2 style="text-align: center;">Grading Labels 分级标签</h2>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td style="width: 30%;">
-                                        <img src="https://speedjin.com/tianma/php/'.$file.'" heigth="auto" width="50%" class="center"/>
-                                    </td>
-                                    <td style="width: 70%;">
-                                        <table class="table-bordered" style="width:100%">
-                                            <tr>
-                                                <td>
-                                                    <p style="font-size: 14px;">Lot No <br>批号</p>
-                                                </td>
-                                                <td>
-                                                    <p style="font-size: 14px;">'.$newLotNo[$i].'</p>
-                                                </td>
-                                                <td>
-                                                    <p style="font-size: 14px;">Grading Gross weight<br>分级毛重,g</p>
-                                                </td>
-                                                <td>
-                                                    <p style="font-size: 14px;">'.$newGrossWeight[$i].'</p>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td>
-                                                    <p style="font-size: 14px;">Box/tray no<br>桶/托盘代号</p>
-                                                </td>
-                                                <td>
-                                                    <p style="font-size: 14px;">'.$newTrayNo[$i].'</p>
-                                                </td>
-                                                <td>
-                                                    <p style="font-size: 14px;">Box/tray weight,g<br>桶/托盘重量</p>
-                                                </td>
-                                                <td>
-                                                    <p style="font-size: 14px;">'.$newTrayWeight[$i].'</p>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td>
-                                                    <p style="font-size: 14px;">Grade <br>等级</p>
-                                                </td>
-                                                <td>
-                                                    <p style="font-size: 14px;">'.$newGrade[$i].'</p>
-                                                </td>
-                                                <td>
-                                                    <p style="font-size: 14px;">Grading Net weight<br>分级净重,g</p>
-                                                </td>
-                                                <td>
-                                                    <p style="font-size: 14px;">'.$newNetWeight[$i].'</p>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td>
-                                                    <p style="font-size: 14px;">Qty <br>片数(pcs)</p>
-                                                </td>
-                                                <td>
-                                                    <p style="font-size: 14px;">'.$qty[$i].'</p>
-                                                </td>
-                                                <td>
-                                                    <p style="font-size: 14px;">Moisture after grading<br>分级后湿度(%)</p>
-                                                </td>
-                                                <td>
-                                                    <p style="font-size: 14px;">'.$moistureAfGrade[$i].'</p>
-                                                </td>
-                                            </tr>
-                                        </table>
-                                    </td>
-                                </tr>
-                            </table>';
+                        if ($update_stmt = $db->prepare("SELECT * FROM grades WHERE id=?")) {
+                            $update_stmt->bind_param('s', $newGrade[$i]);
+                            
+                            // Execute the prepared query.
+                            if ($update_stmt->execute()) {
+                                $result = $update_stmt->get_result();
 
-                $count++;
+                                if($row = $result->fetch_assoc()){
+                                    $message = '<table style="width: 640px;height: 27px;">
+                                    <tr>
+                                        <td colspan="2">
+                                            <h2 style="text-align: center;">Grading Labels 分级标签</h2>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td style="width: 30%;">
+                                            <img src="https://speedjin.com/tianma/php/'.$file.'" heigth="auto" width="50%" class="center"/>
+                                        </td>
+                                        <td style="width: 70%;">
+                                            <table class="table-bordered" style="width:100%">
+                                                <tr>
+                                                    <td>
+                                                        <p style="font-size: 14px;">Lot No <br>批号</p>
+                                                    </td>
+                                                    <td>
+                                                        <p style="font-size: 14px;">'.$newLotNo[$i].'</p>
+                                                    </td>
+                                                    <td>
+                                                        <p style="font-size: 14px;">Grading Gross weight<br>分级毛重,g</p>
+                                                    </td>
+                                                    <td>
+                                                        <p style="font-size: 14px;">'.$newGrossWeight[$i].'</p>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td>
+                                                        <p style="font-size: 14px;">Box/tray no<br>桶/托盘代号</p>
+                                                    </td>
+                                                    <td>
+                                                        <p style="font-size: 14px;">'.$newTrayNo[$i].'</p>
+                                                    </td>
+                                                    <td>
+                                                        <p style="font-size: 14px;">Box/tray weight,g<br>桶/托盘重量</p>
+                                                    </td>
+                                                    <td>
+                                                        <p style="font-size: 14px;">'.$newTrayWeight[$i].'</p>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td>
+                                                        <p style="font-size: 14px;">Grade <br>等级</p>
+                                                    </td>
+                                                    <td>
+                                                        <p style="font-size: 14px;">'.$newGrade[$i].'</p>
+                                                    </td>
+                                                    <td>
+                                                        <p style="font-size: 14px;">Grading Net weight<br>分级净重,g</p>
+                                                    </td>
+                                                    <td>
+                                                        <p style="font-size: 14px;">'.$newNetWeight[$i].'</p>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td>
+                                                        <p style="font-size: 14px;">Qty <br>片数(pcs)</p>
+                                                    </td>
+                                                    <td>
+                                                        <p style="font-size: 14px;">'.$qty[$i].'</p>
+                                                    </td>
+                                                    <td>
+                                                        <p style="font-size: 14px;">Moisture after grading<br>分级后湿度(%)</p>
+                                                    </td>
+                                                    <td>
+                                                        <p style="font-size: 14px;">'.$moistureAfGrade[$i].'</p>
+                                                    </td>
+                                                </tr>
+                                            </table>
+                                        </td>
+                                    </tr>
+                                </table>';
+
+                    $count++;
+                                }
                             }
                         }
                     }
